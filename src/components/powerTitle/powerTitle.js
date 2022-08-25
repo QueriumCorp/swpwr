3fea2ab79b80f21ea2370c2f8ec40205a3371c4b
JVR <vrracing@winning.com>
2022-08-25 18:04:23 -0400

    Fixed indexing problem with POWER letter highlighting

--- src/components/powerTitle/powerTitle.js
+++ src/components/powerTitle/powerTitle.js
@@ -12,6 +12,9 @@ function PowerTitle(props) {
   const title = problem.stepsMnemonic;
   const subTitle = problem.steps[activeStep].label;
   const instructions = problem.steps[activeStep].instruction;
+  const mnemonicIndex = problem.steps[activeStep].mnemonicIndex;
+
+  console.info(activeStep, title, subTitle, instructions, mnemonicIndex);
 
   return (
     <div className="powerTitle">
@@ -20,7 +23,7 @@ function PowerTitle(props) {
           <span
             aria-hidden="true"
             key={index}
-            className={index === activeStep ? "powerTitleCurrentLetter" : ""}
+            className={index === mnemonicIndex ? "powerTitleCurrentLetter" : ""}
           >
             {char}
           </span>
