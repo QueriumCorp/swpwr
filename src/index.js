a0d41351af2d7d0b16b34d9d5241bd2c691cf25d
JVR <vrracing@winning.com>
2022-09-04 20:24:37 -0400

    HolyGrail seems to be working

--- src/index.js
+++ src/index.js
@@ -3,19 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import SWPower from "./SWPower";
 
-ReactDOM.render(
-  <div
-    style={{
-      position: "relative",
-      backgroundColor: "#282c34",
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
-  >
-    <SWPower />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<SWPower />, document.getElementById("root"));
