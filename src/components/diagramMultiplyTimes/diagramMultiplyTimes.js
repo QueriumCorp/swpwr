2b6bce5b8d1f608eba2a037786a4c052e2c782a3
JVR <vrracing@winning.com>
2022-09-08 10:43:35 -0400

    Moved the drawing out as a component

--- src/components/diagramMultiplyTimes/diagramMultiplyTimes.js
+++ src/components/diagramMultiplyTimes/diagramMultiplyTimes.js
@@ -4,6 +4,7 @@ import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./diagramMultiplyTimes.css";
+import DiagramMultiplyTimesWidget from "./diagramMultiplyTimesWidget";
 
 function DiagramMultiplyTimes(props) {
   const selected = props.selected;
@@ -24,50 +25,7 @@ function DiagramMultiplyTimes(props) {
             <p>Are amounts/sets compared a number of times?</p>
           </div>
           <div className="diagramExample">
-            <div className="diagramMultipleBox">
-              <div className="diagramMultipleItem diagramMultipleStart">
-                <div className="diagramMultipleIcon">
-                  <svg
-                    version="1.1"
-                    viewBox="0 0 100 100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <ellipse
-                      cx="50"
-                      cy="50"
-                      rx="47.015"
-                      ry="47.015"
-                      fill="#d5d3d3"
-                      stroke="#000"
-                    />
-                  </svg>
-                </div>
-                <div className="diagramMultipleLabel">Sets</div>{" "}
-              </div>
-              <div>X</div>
-              <div className="diagramMultipleItem diagramMultipleMultiple">
-                Multiplier
-              </div>
-              <div> = </div>
-              <div className="diagramMultipleItem diagramMultipleEnd">
-                <div className="diagramMultipleIcon">
-                  <svg
-                    version="1.1"
-                    viewBox="0 0 100 100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      transform="matrix(.14629 1.284 -1.284 .14629 109.51 -13.316)"
-                      d="m72.025 92.359-60.673-44.835 69.165-30.127-4.2458 37.481z"
-                      fill="#d5d3d3"
-                      stroke="#000"
-                      strokeWidth="1"
-                    />
-                  </svg>
-                </div>
-                <div className="diagramMultipleLabel">Product</div>
-              </div>
-            </div>
+            <DiagramMultiplyTimesWidget />
             <div className="diagramEquation">S X M = P</div>
           </div>
         </Card.Body>
---
+++ src/components/diagramMultiplyTimes/diagramMultiplyTimesWidget.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import "./diagramMultiplyTimes.css";
+
+export default function DiagramMultiplyTimesWidget(props) {
+  const selected = props.selected;
+  const onChange = props.onChange;
+
+  return (
+    <div className="diagramMultipleBox">
+      <div className="diagramMultipleItem diagramMultipleStart">
+        <div className="diagramMultipleIcon">
+          <svg
+            version="1.1"
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <ellipse
+              cx="50"
+              cy="50"
+              rx="47.015"
+              ry="47.015"
+              fill="#d5d3d3"
+              stroke="#000"
+            />
+          </svg>
+        </div>
+        <div className="diagramMultipleLabel">Sets</div>{" "}
+      </div>
+      <div>X</div>
+      <div className="diagramMultipleItem diagramMultipleMultiple">
+        Multiplier
+      </div>
+      <div> = </div>
+      <div className="diagramMultipleItem diagramMultipleEnd">
+        <div className="diagramMultipleIcon">
+          <svg
+            version="1.1"
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              transform="matrix(.14629 1.284 -1.284 .14629 109.51 -13.316)"
+              d="m72.025 92.359-60.673-44.835 69.165-30.127-4.2458 37.481z"
+              fill="#d5d3d3"
+              stroke="#000"
+              strokeWidth="1"
+            />
+          </svg>
+        </div>
+        <div className="diagramMultipleLabel">Product</div>
+      </div>
+    </div>
+  );
+}
