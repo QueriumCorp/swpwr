d082cb3b460631a4a51092daccd393bb742de85e
JVR <vrracing@winning.com>
2022-09-08 09:55:37 -0400

    Moved diagram out into its own control

--- src/components/diagramEqualGroups/diagramEqualGroups.js
+++ src/components/diagramEqualGroups/diagramEqualGroups.js
@@ -3,6 +3,8 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import DiagramEqualGroupsWidget from "./diagramEqualGroupsWidget.js";
+
 import "./diagramEqualGroups.css";
 
 function DiagramEqualGroups(props) {
@@ -25,48 +27,7 @@ function DiagramEqualGroups(props) {
             <p>Are there groups that are all the same size?</p>
           </div>
           <div className="diagramExample">
-            <div className="diagramEqualBox">
-              <div className="diagramEqualItem diagramEqualStart">
-                <div className="diagramEqualIcon">
-                  <svg
-                    version="1.1"
-                    viewBox="0 0 100 100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <ellipse
-                      cx="50"
-                      cy="50"
-                      rx="47.015"
-                      ry="47.015"
-                      fill="#d5d3d3"
-                      stroke="#000"
-                    />
-                  </svg>
-                </div>
-                <div className="diagramEqualLabel">Groups</div>
-              </div>
-              <div>X</div>
-              <div className="diagramEqualItem diagramEqualEqual">Number</div>
-              <div> = </div>
-              <div className="diagramEqualItem diagramEqualEnd">
-                <div className="diagramEqualIcon">
-                  <svg
-                    version="1.1"
-                    viewBox="0 0 100 100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      transform="matrix(.14629 1.284 -1.284 .14629 109.51 -13.316)"
-                      d="m72.025 92.359-60.673-44.835 69.165-30.127-4.2458 37.481z"
-                      fill="#d5d3d3"
-                      stroke="#000"
-                      strokeWidth="1"
-                    />
-                  </svg>
-                </div>
-                <div className="diagramEqualLabel">Product</div>
-              </div>
-            </div>
+            <DiagramEqualGroupsWidget />
             <div className="diagramEquation">G X N = P</div>
           </div>
         </Card.Body>
