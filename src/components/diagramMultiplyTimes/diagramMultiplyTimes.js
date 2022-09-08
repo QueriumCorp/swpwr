9faeda8161e9a9916bea0dc38e66046b76685c86
JVR <vrracing@winning.com>
2022-09-07 20:25:34 -0400

    Multiple Times is stylin

--- src/components/diagramMultiplyTimes/diagramMultiplyTimes.js
+++ src/components/diagramMultiplyTimes/diagramMultiplyTimes.js
@@ -16,24 +16,59 @@ function DiagramMultiplyTimes(props) {
     });
   }
   return (
-    <div className="diagramContainer">
+    <div className="diagramSample">
       <Card onClick={clickHandler}>
-        <Card.Body
-          className={`diagramMultiplyTimes ${selected ? "selected" : ""}`}
-        >
+        <Card.Body className={`diagramCard ${selected ? "selected" : ""}`}>
           <div className="diagramTitle">
-            <h3>MultiplyTimes</h3>
-            <p>Time. Time. Time. What's to become of me?</p>
-            <h2>P1 + P2 = T</h2>
+            <h3>MULTIPLE TIMES</h3>
+            <p>Are amounts/sets compared a number of times?</p>
           </div>
           <div className="diagramExample">
-            <div className="diagramBox">
-              <div className="diagramTop">Total</div>
-              <div className="diagramBottom">
-                <div className="diagramBottomLeft">Part</div>
-                <div className="diagramBottomRight">Part2</div>
+            <div className="diagramMultipleBox">
+              <div className="diagramMultipleItem diagramMultipleStart">
+                <div className="diagramMultipleIcon">
+                  <svg
+                    version="1.1"
+                    viewBox="0 0 100 100"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <ellipse
+                      cx="50"
+                      cy="50"
+                      rx="47.015"
+                      ry="47.015"
+                      fill="#d5d3d3"
+                      stroke="#000"
+                    />
+                  </svg>
+                </div>
+                <div className="diagramMultipleLabel">Sets</div>{" "}
+              </div>
+              <div>X</div>
+              <div className="diagramMultipleItem diagramMultipleMultiple">
+                Multiplier
+              </div>
+              <div> = </div>
+              <div className="diagramMultipleItem diagramMultipleEnd">
+                <div className="diagramMultipleIcon">
+                  <svg
+                    version="1.1"
+                    viewBox="0 0 100 100"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      transform="matrix(.14629 1.284 -1.284 .14629 109.51 -13.316)"
+                      d="m72.025 92.359-60.673-44.835 69.165-30.127-4.2458 37.481z"
+                      fill="#d5d3d3"
+                      stroke="#000"
+                      strokeWidth="1"
+                    />
+                  </svg>
+                </div>
+                <div className="diagramMultipleLabel">Product</div>
               </div>
             </div>
+            <div className="diagramEquation">S X M = P</div>
           </div>
         </Card.Body>
       </Card>
