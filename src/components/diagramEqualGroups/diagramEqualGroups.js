0c5af7f319c1b57a2e109a3d0ea4a45e870fcc09
JVR <vrracing@winning.com>
2022-09-07 20:49:07 -0400

    Styled Equal Groups type

--- src/components/diagramEqualGroups/diagramEqualGroups.js
+++ src/components/diagramEqualGroups/diagramEqualGroups.js
@@ -17,21 +17,57 @@ function DiagramEqualGroups(props) {
   }
 
   return (
-    <div className="diagramEqualGroupsContainer">
+    <div className="diagramSample">
       <Card onClick={clickHandler}>
-        <Card.Body
-          className={`diagramEqualGroups ${selected ? "selected" : ""}`}
-        >
-          <div className="diagramEqualGroupsTitle">
+        <Card.Body className={`diagramCard ${selected ? "selected" : ""}`}>
+          <div className="diagramTitle">
             <h3>EQUAL GROUPS</h3>
-            <p>Equality of Outcomes is silly?</p>
+            <p>Are there groups that are all the same size?</p>
           </div>
-          <div className="diagramEqualGroupsExample">
-            <div className="diagramEqualGroupsBox">
-              <div className="diagramEqualGroupsStart">start</div>
-              <div className="diagramEqualGroupsChange">change</div>
-              <div className="diagramEqualGroupsEnd">end</div>
+          <div className="diagramExample">
+            <div className="diagramEqualBox">
+              <div className="diagramEqualItem diagramEqualStart">
+                <div className="diagramEqualIcon">
+                  <svg
+                    version="1.1"
+                    viewBox="0 0 100 100"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <ellipse
+                      cx="50"
+                      cy="50"
+                      rx="47.015"
+                      ry="47.015"
+                      fill="#d5d3d3"
+                      stroke="#000"
+                    />
+                  </svg>
+                </div>
+                <div className="diagramEqualLabel">Groups</div>
+              </div>
+              <div>X</div>
+              <div className="diagramEqualItem diagramEqualEqual">Number</div>
+              <div> = </div>
+              <div className="diagramEqualItem diagramEqualEnd">
+                <div className="diagramEqualIcon">
+                  <svg
+                    version="1.1"
+                    viewBox="0 0 100 100"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      transform="matrix(.14629 1.284 -1.284 .14629 109.51 -13.316)"
+                      d="m72.025 92.359-60.673-44.835 69.165-30.127-4.2458 37.481z"
+                      fill="#d5d3d3"
+                      stroke="#000"
+                      strokeWidth="1"
+                    />
+                  </svg>
+                </div>
+                <div className="diagramEqualLabel">Product</div>
+              </div>
             </div>
+            <div className="diagramEquation">G X N = P</div>
           </div>
         </Card.Body>
       </Card>
