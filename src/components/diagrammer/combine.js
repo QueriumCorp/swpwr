a1ba57f4b4fa4623a14c8ac25883b3b04d1dd391
JVR <vrracing@winning.com>
2022-09-17 17:09:06 -0400

    1hr adding keypad to Combine

--- src/components/diagrammer/combine.js
+++ src/components/diagrammer/combine.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "react-bootstrap/Card";
 
@@ -8,11 +8,18 @@ import Tags from "./tags";
 
 import "./diagrammer.css";
 import "../diagramCombine/diagramCombine.css";
+import Keypad from "../keypad/keypad";
+import KeyRow from "../keypad/keyrow";
+import Key from "../keypad/key";
+import { BsBackspace } from "react-icons/bs";
+import handleSoftKey from "../../utils/manipulateField.js";
 
 export default function Combine(props) {
   // const problem = props.problem;
   const solution = props.solution;
   const onChange = props.onChange;
+
+  const [focused, setFocused] = useState(null);
 
   const [{ isOverTotal }, totalDrop] = useDrop(() => ({
     accept: ["NUM", "STR"],
@@ -50,6 +57,35 @@ export default function Combine(props) {
   function handleCombinePart2(event) {
     onChange({ type: "combineDiagramPart2", payload: event.target.value });
   }
+
+  function handleFocus(event) {
+    setFocused(event.target || event.srcElement);
+  }
+
+  function handleSoftKeyPress(key) {
+    const result = handleSoftKey(focused, key);
+
+    if (result) {
+      switch (focused.id) {
+        case "start":
+          onChange({ type: "changeDiagramStart", payload: result.newStr });
+          break;
+        case "change":
+          onChange({ type: "changeDiagramChange", payload: result.newStr });
+          break;
+        case "end":
+          onChange({ type: "changeDiagramEnd", payload: result.newStr });
+          break;
+        default:
+          console.error("bad handleSoftKeyPress", key, result, focused);
+      }
+      setTimeout(() => {
+        focused.setSelectionRange(result.newStart, result.newEnd);
+      }, 0);
+    }
+  }
+
+  // JSX
   return (
     <div className="diagramChangeContainer">
       <Card>
@@ -68,6 +104,7 @@ export default function Combine(props) {
                 <input
                   value={solution.diagram.combine.total}
                   onChange={handleCombineTotal}
+                  onFocus={handleFocus}
                   className="inputField"
                 />
               </div>
@@ -81,6 +118,7 @@ export default function Combine(props) {
                   <input
                     value={solution.diagram.combine.part1}
                     onChange={handleCombinePart1}
+                    onFocus={handleFocus}
                     className="inputField"
                   />
                 </div>
@@ -89,10 +127,11 @@ export default function Combine(props) {
                   ref={part2Drop}
                   style={{ background: isOverPart2 ? "red" : "" }}
                 >
-                  Part{" "}
+                  Part
                   <input
                     value={solution.diagram.combine.part2}
                     onChange={handleCombinePart2}
+                    onFocus={handleFocus}
                     className="inputField"
                   />
                 </div>
@@ -100,6 +139,69 @@ export default function Combine(props) {
             </div>
           </div>
           <Tags tags={props.solution.tags}></Tags>
+          <div className="keypadBox">
+            <Keypad
+              className="myKeypad"
+              style={{ minHeight: "300px", minWidth: "200px" }}
+            >
+              <KeyRow>
+                <Key
+                  onClick={handleSoftKeyPress}
+                  retKey="&LARR;"
+                  style={{ background: "orange" }}
+                >
+                  &larr;
+                </Key>
+                <Key
+                  onClick={handleSoftKeyPress}
+                  retKey="&RARR;"
+                  style={{ background: "orange" }}
+                >
+                  &rarr;
+                </Key>
+                <Key
+                  onClick={handleSoftKeyPress}
+                  retKey="&BKSP;"
+                  style={{ background: "orange" }}
+                >
+                  <BsBackspace />
+                </Key>
+              </KeyRow>
+              <KeyRow>
+                <Key onClick={handleSoftKeyPress} retKey="7">
+                  <i>7</i>
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="8">
+                  8
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="9">
+                  9
+                </Key>
+              </KeyRow>
+              <KeyRow>
+                <Key onClick={handleSoftKeyPress} retKey="4">
+                  4
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="5">
+                  5
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="6">
+                  6
+                </Key>
+              </KeyRow>
+              <KeyRow>
+                <Key onClick={handleSoftKeyPress} retKey="1">
+                  1
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="2">
+                  2
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="3">
+                  3
+                </Key>
+              </KeyRow>
+            </Keypad>
+          </div>
         </Card.Body>
       </Card>
     </div>
