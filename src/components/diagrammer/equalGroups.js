73fb9a165b1f9fd3d4f34fa74368233d0b0b8d50
JVR <vrracing@winning.com>
2022-09-17 18:45:39 -0400

    Got circles and triangles to move with input fields

--- src/components/diagrammer/equalGroups.js
+++ src/components/diagrammer/equalGroups.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "react-bootstrap/Card";
 
@@ -8,11 +8,18 @@ import Tags from "./tags";
 
 import "./diagrammer.css";
 import "../diagramEqualGroups/diagramEqualGroups.css";
+import Keypad from "../keypad/keypad";
+import KeyRow from "../keypad/keyrow";
+import Key from "../keypad/key";
+import { BsBackspace } from "react-icons/bs";
+import handleSoftKey from "../../utils/manipulateField.js";
 
 export default function EqualGroups(props) {
   // const problem = props.problem;
   const solution = props.solution;
   const onChange = props.onChange;
+
+  const [focused, setFocused] = useState(null);
 
   const [{ isOverProduct }, productDrop] = useDrop(() => ({
     accept: ["NUM", "STR"],
@@ -50,6 +57,35 @@ export default function EqualGroups(props) {
   function handleGroupsNumber(event) {
     onChange({ type: "groupsDiagramNumber", payload: event.target.value });
   }
+
+  function handleFocus(event) {
+    setFocused(event.target || event.srcElement);
+  }
+
+  function handleSoftKeyPress(key) {
+    const result = handleSoftKey(focused, key);
+
+    if (result) {
+      switch (focused.id) {
+        case "product":
+          onChange({ type: "groupsDiagramProduct", payload: result.newStr });
+          break;
+        case "groups":
+          onChange({ type: "groupsDiagramGroups", payload: result.newStr });
+          break;
+        case "number":
+          onChange({ type: "groupsDiagramNumber", payload: result.newStr });
+          break;
+        default:
+          console.error("bad handleSoftKeyPress", key, result, focused);
+      }
+      setTimeout(() => {
+        focused.setSelectionRange(result.newStart, result.newEnd);
+      }, 0);
+    }
+  }
+
+  //JSX
   return (
     <div className="diagramChangeContainer">
       <Card>
@@ -60,7 +96,7 @@ export default function EqualGroups(props) {
           <div className="diagramExample">
             <div className="diagramMultipleBox">
               <div
-                className="diagramMultipleItem diagramMultipleStart"
+                className="diagramEqualItem diagramMultipleStart"
                 ref={groupsDrop}
                 style={{ background: isOverGroups ? "red" : "" }}
               >
@@ -83,8 +119,10 @@ export default function EqualGroups(props) {
                 <div className="verticalIconLayout">
                   Groups
                   <input
+                    id="groups"
                     value={solution.diagram.groups.groups}
                     onChange={handleGroupsGroups}
+                    onFocus={handleFocus}
                     className="inputField"
                   />
                 </div>
@@ -99,8 +137,10 @@ export default function EqualGroups(props) {
               >
                 Number
                 <input
+                  id="number"
                   value={solution.diagram.groups.number}
                   onChange={handleGroupsNumber}
+                  onFocus={handleFocus}
                   className="inputField"
                 />
               </div>
@@ -108,7 +148,7 @@ export default function EqualGroups(props) {
               <div> = </div>
 
               <div
-                className="diagramMultipleItem diagramMultipleEnd"
+                className="diagramEqualItem diagramMultipleEnd"
                 ref={productDrop}
                 style={{ background: isOverProduct ? "red" : "" }}
               >
@@ -130,55 +170,80 @@ export default function EqualGroups(props) {
                 <div className="verticalIconLayout">
                   Product
                   <input
+                    id="product"
                     value={solution.diagram.groups.product}
                     onChange={handleGroupsProduct}
+                    onFocus={handleFocus}
                     className="inputField"
                   />
                 </div>
               </div>
             </div>
-            {/* <div className="diagramCombineBox">
-              <div
-                className="diagramCombineTop"
-                ref={productDrop}
-                style={{ background: isOverProduct ? "red" : "" }}
-              >
-                Product
-                <input
-                  value={solution.diagram.combine.product}
-                  onChange={handleGroupsProduct}
-                  className="inputField"
-                />
-              </div>
-              <div className="diagramCombineBottom">
-                <div
-                  className="diagramCombineBottomLeft"
-                  ref={groupsDrop}
-                  style={{ background: isOverGroups ? "red" : "" }}
-                >
-                  Part
-                  <input
-                    value={solution.diagram.combine.groups}
-                    onChange={handleGroupsGroups}
-                    className="inputField"
-                  />
-                </div>
-                <div
-                  className="diagramCombineBottomRight"
-                  ref={numberDrop}
-                  style={{ background: isOverNumber ? "red" : "" }}
-                >
-                  Part{" "}
-                  <input
-                    value={solution.diagram.combine.number}
-                    onChange={handleGroupsNumber}
-                    className="inputField"
-                  />
-                </div>
-              </div>
-            </div> */}
           </div>
-          <Tags tags={props.solution.tags}></Tags>
+          <Tags tags={props.solution.tags}></Tags>{" "}
+          <div className="keypadBox">
+            <Keypad
+              className="myKeypad"
+              style={{ minHeight: "300px", minWidth: "200px" }}
+            >
+              <KeyRow>
+                <Key
+                  onClick={handleSoftKeyPress}
+                  retKey="&LARR;"
+                  style={{ background: "orange" }}
+                >
+                  &larr;
+                </Key>
+                <Key
+                  onClick={handleSoftKeyPress}
+                  retKey="&RARR;"
+                  style={{ background: "orange" }}
+                >
+                  &rarr;
+                </Key>
+                <Key
+                  onClick={handleSoftKeyPress}
+                  retKey="&BKSP;"
+                  style={{ background: "orange" }}
+                >
+                  <BsBackspace />
+                </Key>
+              </KeyRow>
+              <KeyRow>
+                <Key onClick={handleSoftKeyPress} retKey="7">
+                  <i>7</i>
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="8">
+                  8
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="9">
+                  9
+                </Key>
+              </KeyRow>
+              <KeyRow>
+                <Key onClick={handleSoftKeyPress} retKey="4">
+                  4
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="5">
+                  5
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="6">
+                  6
+                </Key>
+              </KeyRow>
+              <KeyRow>
+                <Key onClick={handleSoftKeyPress} retKey="1">
+                  1
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="2">
+                  2
+                </Key>
+                <Key onClick={handleSoftKeyPress} retKey="3">
+                  3
+                </Key>
+              </KeyRow>
+            </Keypad>
+          </div>
         </Card.Body>
       </Card>
     </div>
