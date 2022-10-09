29418785d6c97017d5e64446ba5448fc909e2d03
JVR <vrracing@winning.com>
2022-10-09 18:21:39 -0400

    added Review page

--- src/stepViews/reviewerView/reviewerView.js
+++ src/stepViews/reviewerView/reviewerView.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useWizard } from "react-use-wizard";
+import Toast from "react-bootstrap/Toast";
+
+import Reviewer from "../../components/reviewer/reviewer";
+import Stimulator from "../../components/stimulator/stimulator";
 
 import "./reviewerView.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-function ReviewerView(props) {
-  return <div className="Reviewer">{"Reviewer"}</div>;
+export default function ReviewerView(props) {
+  const solution = props.solution;
+  const onChange = props.onChange;
+
+  const [showToast, setShowToast] = useState(false);
+
+  const toggleToast = () => setShowToast(!showToast);
+  const { handleStep } = useWizard();
+
+  handleStep(() => {
+    if (!solution.explanation.length) {
+      toggleToast();
+      throw "Don't know where to catch this. If I throw an error object, the app crashes.  This causes an error in the console, but allows me to display the toast and prevent going to next page."; // eslint-disable-line no-throw-literal
+    }
+  });
+
+  return (
+    <div className="Reviewer">
+      <Stimulator text={props.problem.stimulus} enabled={false}></Stimulator>
+      <div className="DiagramAnalyze">
+        <Toast show={showToast} onClose={toggleToast}>
+          <Toast.Header>
+            <strong className="me-auto">Does your Answer Make Sense?</strong>
+          </Toast.Header>
+          <Toast.Body>You must explain how your answer makes sense!</Toast.Body>
+        </Toast>
+        <DndProvider backend={HTML5Backend}>
+          <Reviewer solution={solution} onChange={onChange}></Reviewer>
+        </DndProvider>
+      </div>
+    </div>
+  );
 }
-
-export default ReviewerView;
