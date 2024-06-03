63e0dbc1ad1c8ae29b0f4cf8ac0e5bfd589314f9
Jim Van Riper <james.vanriper@gmail.com>
2024-06-03 11:49:35 -0400

    added rollup-preserve-directives

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import path from "path";
 import { defineConfig } from "vite";
+
+// Plugins
 import react from "@vitejs/plugin-react";
+import preserveDirectives from "rollup-preserve-directives";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preserveDirectives()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
