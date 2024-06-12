c1c1d009332e267d639056e1aef225ee41a6d5b0
Jim Van Riper <james.vanriper@gmail.com>
2024-06-12 14:42:33 -0400

    Added sample props

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,36 @@ const swapiUrl = import.meta.env.DEV
 console.info("ENVIRONMENT", import.meta.env);
 console.info("SWAPI URL", swapiUrl);
 
+const problem = {
+  appKey: "JiraTestPage",
+  policyId: "$A1$",
+  problemId: "QUES6018",
+  title: "Solve compound linear inequalities in 1 variable",
+  stimulus:
+    "Minh spent $6.25 on 5 sticker books to give his nephews. Find the cost of each sticker book.",
+  topic: "gradeBasicAlgebra",
+  definition:
+    'SolveWordProblemAns[{"Minh spent $6.25 on 5 sticker books to give his nephews. Find the cost of each sticker book."}]',
+  hints: [],
+};
+
+const student = {
+  studentId: "PokeyLoki",
+  studentName: "Loki Van Riper",
+};
+
+const options = {
+  swapiUrl,
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="w-dvw h-dvh overflow-hidden relative">
       <StepWisePower
         className="absolute top-0 left-0 right-0 bottom-0 flex flex-col"
-        swapiUrl={swapiUrl}
+        problem={problem}
+        student={student}
+        options={options}
       />
     </div>
   </React.StrictMode>,
