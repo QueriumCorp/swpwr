ec3c2358fa7c0d95b4e1925f6d8ac491195bdfb4
Jim Van Riper <james.vanriper@gmail.com>
2024-08-16 13:06:13 -0400

    Setup prettier to sort tailwindcss classes

--- src/components/qq/HdrBar.tsx
+++ src/components/qq/HdrBar.tsx
@@ -1,19 +1,19 @@
-import * as React from "react";
+import * as React from 'react'
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils'
 
 export interface HdrBarProps extends React.HTMLAttributes<HTMLDivElement> {
-  highlightLetter?: string;
-  subTitle?: string;
-  instructions?: string;
+  highlightLetter?: string
+  subTitle?: string
+  instructions?: string
 }
 
 const HdrBar = React.forwardRef<HTMLDivElement, HdrBarProps>(
   (
     {
-      highlightLetter = "P",
-      subTitle = "The subtitle",
-      instructions = "Need to provide instructions",
+      highlightLetter = 'P',
+      subTitle = 'The subtitle',
+      instructions = 'Need to provide instructions',
       className,
       ...props
     },
@@ -23,54 +23,55 @@ const HdrBar = React.forwardRef<HTMLDivElement, HdrBarProps>(
       <div
         ref={ref}
         className={cn(
-          "HdrBar  bg-slate-300  min-h-24 w-full border-none relative pl-2 pr-2",
+          'HdrBar',
+          'relative min-h-24 w-full border-none bg-slate-300 pl-2 pr-2',
           className,
         )}
         {...props}
       >
-        <div className="font-inter font-black text-5xl select-none">
+        <div className="select-none font-inter text-5xl font-black">
           <span
             className={cn(
-              highlightLetter === "P" ? "text-qqAccent" : "text-slate-500",
+              highlightLetter === 'P' ? 'text-qqAccent' : 'text-slate-500',
             )}
           >
             P
           </span>
           <span
             className={cn(
-              highlightLetter === "O" ? "text-qqAccent" : "text-slate-500",
+              highlightLetter === 'O' ? 'text-qqAccent' : 'text-slate-500',
             )}
           >
             O
           </span>
           <span
             className={cn(
-              highlightLetter === "W" ? "text-qqAccent" : "text-slate-500",
+              highlightLetter === 'W' ? 'text-qqAccent' : 'text-slate-500',
             )}
           >
             W
           </span>
           <span
             className={cn(
-              highlightLetter === "E" ? "text-qqAccent" : "text-slate-500",
+              highlightLetter === 'E' ? 'text-qqAccent' : 'text-slate-500',
             )}
           >
             E
           </span>
           <span
             className={cn(
-              highlightLetter === "R" ? "text-qqAccent" : "text-slate-500",
+              highlightLetter === 'R' ? 'text-qqAccent' : 'text-slate-500',
             )}
           >
             R
           </span>
         </div>
-        <div className="font-inter text-1xl select-none">{subTitle}</div>
-        <div className="font-sura text-2xl select-none">{instructions}</div>
+        <div className="text-1xl select-none font-inter">{subTitle}</div>
+        <div className="select-none font-sura text-2xl">{instructions}</div>
       </div>
-    );
+    )
   },
-);
-HdrBar.displayName = "HdrBar";
+)
+HdrBar.displayName = 'HdrBar'
 
-export { HdrBar };
+export { HdrBar }
