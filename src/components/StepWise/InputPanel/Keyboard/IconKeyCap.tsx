795f21fe5324b94805f87394f8986de695434753
Jim Van Riper <james.vanriper@gmail.com>
2024-09-03 12:43:01 -0400

    Fixed multiplication and division keycaps

--- src/components/StepWise/InputPanel/Keyboard/IconKeyCap.tsx
+++ src/components/StepWise/InputPanel/Keyboard/IconKeyCap.tsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { Operator } from "../../stores/solution";
-import { RxThickArrowLeft, RxThickArrowRight } from "react-icons/rx";
-import { PiBackspaceLight } from "react-icons/pi";
-import { TiDivideOutline } from "react-icons/ti";
-import { FaRegTrashCan } from "react-icons/fa6";
-import { FaPlus } from "react-icons/fa";
-import { RiSpace } from "react-icons/ri";
-import { GrSubtract } from "react-icons/gr";
-import { FaEquals } from "react-icons/fa";
+import React from 'react'
+import { Operator } from '../../stores/solution'
+import { RxThickArrowLeft, RxThickArrowRight } from 'react-icons/rx'
+import { PiBackspaceLight } from 'react-icons/pi'
+import { TiDivide } from 'react-icons/ti'
+import { TiTimes } from 'react-icons/ti'
+import { FaRegTrashCan } from 'react-icons/fa6'
+import { FaPlus } from 'react-icons/fa'
+import { RiSpace } from 'react-icons/ri'
+import { GrSubtract } from 'react-icons/gr'
+import { FaEquals } from 'react-icons/fa'
 type IconKeyCapProps = {
-  operator: Operator;
-};
+  operator: Operator
+}
 
 export const IconKeyCap = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & IconKeyCapProps
 >(({ operator }, _ref) => {
   return (
-    <div style={{ userSelect: "none", ...operator.symbol_style }}>
+    <div style={{ userSelect: 'none', ...operator.symbol_style }}>
       {renderIcon(operator.symbol_icon)}
     </div>
-  );
-});
+  )
+})
 
 // Radix React Icons - https://www.radix-ui.com/icons
 // The tooltip names map to the component names
@@ -29,25 +30,27 @@ export const IconKeyCap = React.forwardRef<
 // note: The icon library is prefixed so we can use others in the future
 function renderIcon(icon: string) {
   switch (icon) {
-    case "radix:thickArrowLeft":
-      return <RxThickArrowLeft />;
-    case "radix:thickArrowRight":
-      return <RxThickArrowRight />;
-    case "PiBackspaceLight":
-      return <PiBackspaceLight />;
-    case "TiDivideOutline":
-      return <TiDivideOutline />;
-    case "FaRegTrashCan":
-      return <FaRegTrashCan />;
-    case "FaPlus":
-      return <FaPlus />;
-    case "RiSpace":
-      return <RiSpace />;
-    case "GrSubtract":
-      return <GrSubtract />;
-    case "FaEquals":
-      return <FaEquals />;
+    case 'radix:thickArrowLeft':
+      return <RxThickArrowLeft />
+    case 'radix:thickArrowRight':
+      return <RxThickArrowRight />
+    case 'PiBackspaceLight':
+      return <PiBackspaceLight />
+    case 'TiDivide':
+      return <TiDivide />
+    case 'TiTimes':
+      return <TiTimes />
+    case 'FaRegTrashCan':
+      return <FaRegTrashCan />
+    case 'FaPlus':
+      return <FaPlus />
+    case 'RiSpace':
+      return <RiSpace />
+    case 'GrSubtract':
+      return <GrSubtract />
+    case 'FaEquals':
+      return <FaEquals />
     default:
-      return <span>💩</span>;
+      return <span>💩</span>
   }
 }
