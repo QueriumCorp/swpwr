bcfdb94330c89c1db01c8d6206d565a069601aba
Jim Van Riper <james.vanriper@gmail.com>
2024-09-19 11:18:20 -0400

    Updated organize to support positional accuracy in qEval

--- src/store/submitOrganize.ts
+++ src/store/submitOrganize.ts
@@ -1,26 +1,45 @@
-import type { SetFn, GetFn } from "./_types";
+import type { SetFn, GetFn } from './_types'
+
+type varAss = {
+  variable: string
+  value: string | null
+}
 
 const submitOrganize = async (_set: SetFn, get: GetFn, equation: string) => {
-  const problem = get().problem;
-  const session = get().session;
+  const problem = get().problem
+  const session = get().session
+  const varAssignments: varAss[] = []
+  session.identifiers.map((variable, index) => {
+    varAssignments.push({
+      variable: variable,
+      value:
+        session.identifiers[index] == session.schemaValues[index]
+          ? null
+          : session.schemaValues[index],
+    })
+  })
+
+  console.log(varAssignments)
+  console.log(JSON.stringify(varAssignments))
 
   const body = {
     appKey: problem.appKey,
     sessionToken: session.sessionToken,
     equation: equation,
-  };
+    varAssignments: varAssignments,
+  }
 
-  const response = await fetch(get().swapiUrl + "/submitOrganize/", {
-    method: "POST",
+  const response = await fetch(get().swapiUrl + '/submitOrganize/', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(body),
-  });
-  const data = await response.json();
+  })
+  const data = await response.json()
 
-  get().logAction(`submitOrganize: ${response.status} ${JSON.stringify(data)}`);
-  return data;
-};
+  get().logAction(`submitOrganize: ${response.status} ${JSON.stringify(data)}`)
+  return data
+}
 
-export default submitOrganize;
+export default submitOrganize
