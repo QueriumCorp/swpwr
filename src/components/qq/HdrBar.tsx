90bb9ce7e0eb85652a539233ec5b11480432c970
Jim Van Riper <james.vanriper@gmail.com>
2024-09-27 12:46:43 -0400

    shorter header with chatty toggle icon

--- src/components/qq/HdrBar.tsx
+++ src/components/qq/HdrBar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
+import { SpeakerLoudIcon } from '@radix-ui/react-icons'
 
 export interface HdrBarProps extends React.HTMLAttributes<HTMLDivElement> {
   highlightLetter?: string
@@ -24,50 +25,70 @@ const HdrBar = React.forwardRef<HTMLDivElement, HdrBarProps>(
         ref={ref}
         className={cn(
           'HdrBar',
-          'relative min-h-24 w-full border-none bg-slate-300 pl-2 pr-2',
+          'relative w-full border-none bg-slate-300 pl-2 pr-2',
+          'grid grid-cols-3 gap-4',
           className,
         )}
         {...props}
       >
-        <div className="select-none font-inter text-5xl font-black">
-          <span
-            className={cn(
-              highlightLetter === 'P' ? 'text-qqAccent' : 'text-slate-500',
-            )}
-          >
-            P
-          </span>
-          <span
-            className={cn(
-              highlightLetter === 'O' ? 'text-qqAccent' : 'text-slate-500',
-            )}
-          >
-            O
-          </span>
-          <span
-            className={cn(
-              highlightLetter === 'W' ? 'text-qqAccent' : 'text-slate-500',
-            )}
-          >
-            W
-          </span>
-          <span
-            className={cn(
-              highlightLetter === 'E' ? 'text-qqAccent' : 'text-slate-500',
-            )}
-          >
-            E
-          </span>
-          <span
-            className={cn(
-              highlightLetter === 'R' ? 'text-qqAccent' : 'text-slate-500',
-            )}
-          >
-            R
-          </span>
+        <div>
+          <div className="flex select-none font-inter font-black">
+            <div
+              className={cn(
+                highlightLetter === 'P'
+                  ? 'text-6xl text-qqAccent'
+                  : 'text-5xl text-slate-500',
+              )}
+            >
+              P
+            </div>
+            <div
+              className={cn(
+                highlightLetter === 'O'
+                  ? 'text-6xl text-qqAccent'
+                  : 'text-5xl text-slate-500',
+              )}
+            >
+              O
+            </div>
+            <div
+              className={cn(
+                highlightLetter === 'W'
+                  ? 'text-6xl text-qqAccent'
+                  : 'text-5xl text-slate-500',
+              )}
+            >
+              W
+            </div>
+            <div
+              className={cn(
+                highlightLetter === 'E'
+                  ? 'text-6xl text-qqAccent'
+                  : 'text-5xl text-slate-500',
+              )}
+            >
+              E
+            </div>
+            <div
+              className={cn(
+                highlightLetter === 'R'
+                  ? 'text-6xl text-qqAccent'
+                  : 'text-5xl text-slate-500',
+              )}
+            >
+              R
+            </div>
+          </div>
+          <div className="text-1xl mt-[-10px] select-none font-inter">
+            {subTitle}
+          </div>
         </div>
-        <div className="text-1xl select-none font-inter">{subTitle}</div>
-        <div className="select-none font-sura text-2xl">{instructions}</div>
+        <div className="flex select-none items-center justify-center font-capriola text-2xl text-qqBrand">
+          {instructions}
+        </div>
+        <div className="flex select-none items-center justify-end font-capriola text-2xl text-black">
+          <SpeakerLoudIcon className="mr-2 inline-block h-6 w-6" />
+        </div>
       </div>
     )
   },
