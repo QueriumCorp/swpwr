46b184fb67eeb063b855717db946bc1d47f0a099
Jim Van Riper <james.vanriper@gmail.com>
2024-11-10 09:27:45 -0500

    Added new wholeword function but matching needs work

--- src/components/qq/StimulusSelector/functions/FindHighlightIndeces.ts
+++ src/components/qq/StimulusSelector/functions/FindHighlightIndeces.ts
@@ -1,5 +1,5 @@
 import { type Highlight } from '@/store/_types'
-import { findIndexes } from './findIndeces'
+import { findIndexes, findWholeWordPositions } from './findIndeces'
 
 // TODO: We could make this a lot more sophissticated by filtering out
 // the highlights by the sentence that contains the caretOffset.
@@ -144,6 +144,151 @@ export function findHighlightIndeces(
   }
   return { start, end, bestHighlight }
 }
+
+// export function findHighlightIndeces(
+//   caretOffset: number,
+//   sentence: string,
+//   highlights: Highlight[],
+// ) {
+//   // if no caretOffset, return
+//   if (caretOffset === -1 || highlights.length === 0) {
+//     return { start: 0, end: Infinity, highlight: '' }
+//   }
+
+//   let start = 0,
+//     end: number = 0
+//   let highlight: Highlight | null = null
+
+//   if (highlights.length === 0 || !sentence) {
+//     return { start: 0, end: Infinity, highlight: '' }
+//   }
+
+//   // score each highlight for distance from caretOffset.
+//   // 0 is exact match while Infinity is no match.
+//   // Other values are character distance from caretOffset.
+//   const scores = highlights.map(element => {
+//     // highlight is a string
+//     if (typeof element === 'string') {
+//       let matches = findWholeWordPositions(sentence, element)
+//       // let matches = findIndexes(sentence, element)
+//       if (matches.length == 1) {
+//         return Math.abs(matches[0].index - caretOffset)
+//       } else if (matches.length > 1) {
+//         // find the closest match
+//         let closest = Infinity
+//         for (let i = 0; i < matches.length; i++) {
+//           let distance = Math.abs(matches[i].index - caretOffset)
+//           if (distance < closest) {
+//             closest = distance
+//           }
+//         }
+//         return closest
+//       } else {
+//         return Infinity
+//       }
+//     }
+
+//     //  highlight is an array of two strings
+//     else if (Array.isArray(element)) {
+//       let matches0 = findWholeWordPositions(sentence, element[0])
+//       let matches1 = findWholeWordPositions(sentence, element[1])
+//       // let matches0 = findIndexes(sentence, element[0])
+//       // let matches1 = findIndexes(sentence, element[1])
+
+//       if (matches0.length == 0 || matches1.length == 0) {
+//         // at least one wasn't found
+//         return Infinity
+//       } else {
+//         // find the closest match for match0
+//         let closest0 = Infinity
+//         for (let i = 0; i < matches0.length; i++) {
+//           let distance = Math.abs(matches0[i].index - caretOffset)
+//           if (distance < closest0) {
+//             closest0 = distance
+//           }
+//         }
+//         // find the closest match for match0
+//         let closest1 = Infinity
+//         for (let i = 0; i < matches1.length; i++) {
+//           let distance = Math.abs(matches1[i].index - caretOffset)
+//           if (distance < closest1) {
+//             closest1 = distance
+//           }
+//         }
+//         return Math.min(closest0, closest1)
+//       }
+//     }
+//     return Infinity
+//   })
+
+//   console.info('caretOffset:', caretOffset)
+//   highlights.forEach((element, index) => {
+//     console.info(JSON.stringify(element), scores[index])
+//   })
+
+//   if (!scores) {
+//     return { start: 0, end: Infinity, highlight: '' }
+//   }
+
+//   const bestHighlight = highlights[indexOfSmallest(scores)]
+//   console.info('bestHighlight', bestHighlight)
+
+//   // Now find the start and end of the highlight
+//   if (typeof bestHighlight === 'string') {
+//     let matches = findWholeWordPositions(sentence, bestHighlight)
+//     // let matches = findIndexes(sentence, bestHighlight)
+//     if (matches.length == 1) {
+//       start = matches[0].index
+//       end = start + bestHighlight.length
+//     } else if (matches.length > 1) {
+//       // find the closest match
+//       let closest = Infinity
+//       let bestMatch = 0
+//       for (let i = 0; i < matches.length; i++) {
+//         let distance = Math.abs(matches[i].index - caretOffset)
+//         if (distance < closest) {
+//           closest = distance
+//           bestMatch = i
+//         }
+//       }
+//       start = matches[0].index
+//       end = start + bestHighlight.length
+//     }
+//   } else {
+//     // find the closest match for match0
+//     let start0 = 0,
+//       end0 = 0
+//     let matches0 = findWholeWordPositions(sentence, bestHighlight[0])
+//     // let matches0 = findIndexes(sentence, bestHighlight[0])
+//     let closest0 = Infinity
+//     for (let i = 0; i < matches0.length; i++) {
+//       let distance = Math.abs(matches0[i].index - caretOffset)
+//       if (distance < closest0) {
+//         closest0 = distance
+//         start0 = matches0[i].index
+//         end0 = start0 + bestHighlight[0].length
+//       }
+//     }
+
+//     // find the closest match for match0
+//     let start1 = 0,
+//       end1 = 0
+//     let matches1 = findWholeWordPositions(sentence, bestHighlight[1])
+//     // let matches1 = findIndexes(sentence, bestHighlight[1])
+//     let closest1 = Infinity
+//     for (let i = 0; i < matches1.length; i++) {
+//       let distance = Math.abs(matches1[i].index - caretOffset)
+//       if (distance < closest1) {
+//         closest1 = distance
+//         start1 = matches1[i].index
+//         end1 = start1 + bestHighlight[1].length
+//       }
+//     }
+//     start = Math.min(start0, start1)
+//     end = Math.max(end0, end1)
+//   }
+//   return { start, end, bestHighlight }
+// }
 
 function indexOfSmallest(arr: number[]) {
   if (arr.length === 0) {
